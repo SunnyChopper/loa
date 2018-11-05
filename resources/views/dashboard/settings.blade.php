d81cf289f065d084d41f833b70f44164ff95d04d
SunnyChopper <ishy.singh@gmail.com>
2018-11-04 20:49:38 -0600

    Under construction messages

--- resources/views/dashboard/settings.blade.php
+++ resources/views/dashboard/settings.blade.php
@@ -4,7 +4,9 @@
 	@include('layouts.hero')
 	<div class="container mt-32 mb-32">
 		<div class="row">
-
+			<div class="well">
+				<h4 class="text-center">Feature coming soon...</h4>
+			</div>
 		</div>
 	</div>
 @endsection
