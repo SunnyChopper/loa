1078a7365bc1b62c25fdd444fa81e69f01562323
SunnyChopper <ishy.singh@gmail.com>
2018-11-04 21:12:15 -0600

    HTTPS request

--- resources/views/layouts/js.blade.php
+++ resources/views/layouts/js.blade.php
@@ -1,5 +1,5 @@
 <script src="{{ URL::asset('js/vendor/jquery-2.2.4.min.js') }}"></script>
-<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.7/jquery.validate.min.js"></script>
+<script type="text/javascript" src="https://ajax.aspnetcdn.com/ajax/jquery.validate/1.7/jquery.validate.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="{{ URL::asset('js/vendor/bootstrap.min.js') }}"></script>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
