88190c3bd1d389d5770c1d3efa858a5909579558
Sunny Singh <ishy.singh@gmail.com>
2018-12-24 00:19:51 -0600

    Fixed gitignore

--- storage/framework/views/c7f5034d1f854d5cfd21094bd17d3e63346c6c77.php
+++
@@ -1,9 +0,0 @@
-<?php $__env->startSection('content'); ?>
-	<?php echo $__env->make('layouts.hero', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
-	<div class="container mt-32 mb-32">
-		<div class="row">
-
-		</div>
-	</div>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
